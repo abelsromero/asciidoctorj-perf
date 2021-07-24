fcfda6d93133e274ea38fa8e65f82ca74dbd1543
Abel Salgado Romero <abelromero@gmail.com>
2021-07-24 21:40:00 +0200

    Add timestamp to timing files

--- runner-java.sh
+++ runner-java.sh
@@ -3,11 +3,13 @@
 ./gradlew clean installDist
 
 TIMEFORMAT=%R
+
 i=0
-limit=50
+readonly limit=50
+readonly PREFIX=$(date +"%y%m%d-%H%M%S")  
 
 while [ $i -le $limit ]
 do
- (time ./converter/build/install/converter/bin/converter pdf converter/web-application-reference.adoc) 1>> output_java.txt 2>> output_java_total.txt
+ (time ./converter/build/install/converter/bin/converter pdf converter/web-application-reference.adoc) 1>> "${PREFIX}_java_convert_times.txt" 2>> "${PREFIX}_java_total_jvm_times.txt"
  ((i++))
 done;
--- runner-ruby.sh
+++ runner-ruby.sh
@@ -2,10 +2,12 @@
 
 TIMEFORMAT=%R
 i=0
-limit=50
+
+readonly limit=50
+readonly PREFIX=$(date +"%y%m%d-%H%M%S")
 
 while [ $i -le $limit ]
 do
- (time asciidoctor-pdf converter/web-application-reference.adoc -o build/generated.pdf) 2>> output_ruby.txt
+ (time asciidoctor-pdf converter/web-application-reference.adoc -o build/generated.pdf) 2>> "${PREFIX}_ruby_times.txt"
  ((i++))
 done;
